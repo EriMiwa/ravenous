a9e57722a954a1af5bbbb03feba8d7ca9287b398
Erika Miwa <erikamiwa612@gmail.com>
2019-09-13 20:25:30 -0700

    add handler function for SearchBar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -25,15 +25,21 @@ const businesses = [
   business,
 ];
 
-export default class App extends React.Component {
+class App extends React.Component {
+
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`)
+  }
 
   render() {
     return (
       <div className="App">
         <h1>ravenous</h1>
-        <SearchBar />
+        <SearchBar searchYelp={this.searchYelp}/>
         <BusinessList businesses={businesses}/> 
     </div>
     );
   }
 }
+
+export default App;
